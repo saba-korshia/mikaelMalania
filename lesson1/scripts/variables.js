5d7386952c5786bf0e7b324531f86d4ee211454a
Mikael Malania <m.malania@noxtton.com>
2023-11-18 16:58:09 +0400

    feat: added lesson 1 files

---
+++ lesson1/scripts/variables.js
@@ -0,0 +1,13 @@
+// 
+
+const age = 25;
+
+let firstName = "Giorgi";
+
+var lastName = "Kakauridze";
+
+let fullName = "123var";
+
+let userInput = prompt("Enter your name");
+
+alert("Hello " + userInput);
